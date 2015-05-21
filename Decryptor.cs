218fd27f0b0aded7d9b84f1ac552b430f4dbd7f8
Eugene Rogolev <iargentumi@gmail.com>
2015-05-21 23:10:13 +0600

    Revert "Revert "Enigma Lab""
    This reverts commit e2c42e72931c7128804771bf16b667e19bf38f05.

---
+++ Decryptor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Rogolev.Nsudotnet.Enigma
+{
+    class Decryptor
+    {
+        public static void DecryptFile(SymmetricAlgorithm algo, string inFileName, string keyFileName, string outFileName)
+        {
+            using (FileStream inStream = new FileStream(inFileName, FileMode.Open, FileAccess.Read),
+                outStream = new FileStream(outFileName, FileMode.Create, FileAccess.Write),
+                keyStream = new FileStream(keyFileName, FileMode.Open, FileAccess.Read))
+            {
+                var keyReader = new BinaryReader(keyStream);
+                var iv = Convert.FromBase64String(keyReader.ReadString());
+                var sKey = Convert.FromBase64String(keyReader.ReadString());
+                algo.Key = sKey;
+                algo.IV = iv;
+                var decryptor = algo.CreateDecryptor();
+                var cryptoStream = new CryptoStream(outStream, decryptor, CryptoStreamMode.Write);
+                inStream.CopyTo(cryptoStream);
+            }
+        }
+    }
+}
